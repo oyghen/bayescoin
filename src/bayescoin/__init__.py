daf3f5e974b705765e83626782c4c64526668711
oyghen <oyghen@duck.com>
2025-12-11 08:37:56 +0100

    feat(viz): add plot

--- src/bayescoin/__init__.py
+++ src/bayescoin/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ("__version__", "BetaShape")
+__all__ = ("__version__", "BetaShape", "plot")
 
 from importlib import metadata
 
 from bayescoin.core import BetaShape
+from bayescoin.viz import plot
 
 __version__ = metadata.version(__name__)
